785d88eae57bf068f446a8001b2274031b4a9aec
Gábor <makgab@gmail.com>
2021-07-19 18:21:06 +0200

    Update
    Update files

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 // src/Controller/LuckyController.php
 namespace App\Controller;
@@ -34,40 +33,3 @@ class LuckyController extends AbstractController
     }
 }
 
-=======
-<?php
-// src/Controller/LuckyController.php
-namespace App\Controller;
-
-use Symfony\Component\HttpFoundation\Response;
-
-
-// for annotations
-use Symfony\Component\Routing\Annotation\Route;
-
-// for AbstractController
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-
-
-
-class LuckyController extends AbstractController
-{
-    /**
-     * @Route("/lucky/number")
-     */
-    public function number(): Response
-    {
-        $number = random_int(0, 100);
-
-        //return new Response(
-        //    '<html><body>Lucky number: '.$number.'</body></html>'
-        //);
-
-        return $this->render('lucky/number.html.twig', [
-            'number' => $number,
-        ]);
-
-    }
-}
-
->>>>>>> 03ba94b208baea597a5c27c50ae3b71571862ad6
